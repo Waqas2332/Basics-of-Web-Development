3abfb4b2a679f18b79c868cd20b2a6f771137a60
waqas <wmunir232@gmail.com>
2024-03-23 15:08:33 +0500

    Added auto expand functionality

--- Mini Projects/50 Basic Projects/01-expanding-cards/script.js
+++ Mini Projects/50 Basic Projects/01-expanding-cards/script.js
@@ -1,8 +1,21 @@
 const panels = document.querySelectorAll(".panel");
 
-panels.forEach((panel) => {
+let counter = 0;
+
+setInterval(() => {
+  removeActiveClasses();
+  panels[counter].classList.add("active");
+  counter++;
+
+  if (counter >= panels.length) {
+    counter = 0;
+  }
+}, 4000);
+
+panels.forEach((panel, index) => {
   panel.addEventListener("click", () => {
     removeActiveClasses();
+    counter = index;
     panel.classList.add("active");
   });
 });
