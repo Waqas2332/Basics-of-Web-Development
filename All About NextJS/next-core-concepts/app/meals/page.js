30d92966269a45820eb1825cfea5e52878b29f90
waqas <wmunir232@gmail.com>
2024-05-06 21:42:28 +0500

    added meal items page

--- All About NextJS/next-core-concepts/app/meals/page.js
+++ All About NextJS/next-core-concepts/app/meals/page.js
@@ -1,5 +1,27 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
 const Mealspage = () => {
-  return <div>Mealspage</div>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Receipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 };
 
 export default Mealspage;
